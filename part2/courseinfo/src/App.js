c552b75374f9bd39f6c5c75e992226676ebdc81b
Eric <ericmiranda7@gmail.com>
2020-11-24 03:03:44 +0530

    courseinfo step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Course from './components/Course'
 
-const App = ({ course }) => {
-    return <Course course={course} />
+const App = ({ courses }) => {
+    return (
+        courses.map(
+            (course) => <Course course={course} />
+        )
+    )
 }
 
 export default App
