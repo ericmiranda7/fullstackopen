0bb5d85897b417cc8d3135344f4211ecf0ec80df
Eric <ericmiranda7@gmail.com>
2020-11-23 22:00:44 +0530

    courseinfo step7, 8

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,19 +2,24 @@ import React from 'react'
 
 const Header = ({ text }) => <h1>{text}</h1>
 
-const Part = ({text, value}) => <p>{text} {value}</p>
+const Part = ({ text, value }) => <p>{text} {value}</p>
 
-const Content = ({ part }) => {
+const Content = ({ parts }) => {
+    const total = parts.reduce((sum, part) => part.exercises + sum, 0)
+
     return (
-        part.map((part) => <Part key={part.id} text={part.name} value={part.exercises}/>)
+        <div>
+            {parts.map((part) => <Part key={part.id} text={part.name} value={part.exercises} />)}
+            <Part text="Total of " value={total}/>
+        </div>
     )
 }
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
     return (
         <div>
             <Header text={course.name} />
-            <Content part={course.parts} />
+            <Content parts={course.parts} />
         </div>
     )
 }
