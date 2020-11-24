6795d83526d0b653f55f242d1dfaf33374bff4f2
Eric <ericmiranda7@gmail.com>
2020-11-24 19:42:19 +0530

    phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,18 +4,27 @@ const Person = ({ name, number }) => <p>{name} {number}</p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '9822388901'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
 
   const handleNameChange = event => setNewName(event.target.value)
 
   const handleNumberChange = event => setNewNumber(event.target.value)
+
+  const handleSearchChange = event => {
+    setSearchName(event.target.value)
+    event.target.value ?
+      setShowAll(false) :
+      setShowAll(true)
+  }
 
   const handleClick = (event) => {
     event.preventDefault()
@@ -24,13 +33,22 @@ const App = () => {
       number: newNumber,
     }
     persons.find(person => person.name === newName) ?
-    alert(`${newName} is already added to phonebook`) :
-    setPersons(persons.concat(newPerson))
+      alert(`${newName} is already added to phonebook`) :
+      setPersons(persons.concat(newPerson))
   }
+
+
+
+  const personsToShow = showAll ?
+    persons :
+    persons.filter(person => person.name.toLowerCase().includes(searchName))
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <h2>Search</h2>
+      Filter names with <input value={searchName} onChange={handleSearchChange} />
+      <h2>Add new entry</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -41,7 +59,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
+      {personsToShow.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
     </div>
   )
 }
