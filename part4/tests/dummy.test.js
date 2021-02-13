e61f473d0445d46317a359eb9b349637b6baefa0
Eric <ericmiranda7@gmail.com>
2021-02-13 10:45:05 +0530

    4.4 step2

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -7,3 +7,21 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('list with one blog equals likes of it', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+})
