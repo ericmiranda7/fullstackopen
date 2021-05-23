c4058ff2c608aa44e84073d27b444ba6f44db277
Eric Miranda <ericmiranda7@gmail.com>
2021-05-23 16:21:13 +0530

    5.1, step1

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,22 +1,60 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import Login from './components/Login'
+import loginService from './services/login'
+import loginForm from './components/Login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null) 
+  
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      console.log('invalid creds')
+    } 
+  }
+
+  const showBlogs = () => (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <div>{user.name} logged in</div>
+      <br/>
+      {blogs.filter(
+        blog => blog.user.username === user.username
+      ).map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+    {user === null
+      ? <Login
+          handleLogin={handleLogin}
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}  
+        />
+      : showBlogs()}
     </div>
   )
 }
