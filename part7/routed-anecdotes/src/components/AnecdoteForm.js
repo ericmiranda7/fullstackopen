382a43db471e3416e1f4b3393535e03128314ea4
Eric Miranda <ericmiranda7@gmail.com>
2021-06-05 21:35:33 +0530

    7.5 step2

--- part7/routed-anecdotes/src/components/AnecdoteForm.js
+++ part7/routed-anecdotes/src/components/AnecdoteForm.js
@@ -26,6 +26,13 @@ const CreateNew = (props) => {
     )
   }
 
+  const handleReset = (event) => {
+    event.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -43,6 +50,7 @@ const CreateNew = (props) => {
           <input name='info' {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
