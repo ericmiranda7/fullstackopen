657c98169d0f0329cb12fde358a06fb4a0416790
Eric Miranda <ericmiranda7@gmail.com>
2021-07-01 08:40:33 +0530

    7.14 Individual user view

--- part7/bloglist/client/src/App.js
+++ part7/bloglist/client/src/App.js
@@ -6,9 +6,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { clearNotification, setMessage } from './reducers/notificationReducer'
 import { getBlogsFromDb } from './reducers/blogReducer'
 import { setUser } from './reducers/userReducer'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, useRouteMatch, useHistory } from 'react-router-dom'
 import Blogs from './components/Blogs'
 import Users from './components/Users'
+import userService from './services/users'
+import User from './components/User'
 
 const App = () => {
   const user = useSelector(state => state.user)
@@ -16,6 +18,14 @@ const App = () => {
   const [password, setPassword] = useState('')
   const message = useSelector(state => state.notification)
   const dispatch = useDispatch()
+  const [users, setUsers] = useState([])
+  const history = useHistory()
+
+  useEffect(async () => {
+    const users = await userService.getAll()
+    console.log(users)
+    setUsers(users)
+  }, [])
 
   useEffect(() => {
     dispatch(getBlogsFromDb())
@@ -36,6 +46,7 @@ const App = () => {
       dispatch(setUser(user))
       setUsername('')
       setPassword('')
+      history.push('/')
     } catch {
       dispatch(
         setMessage({
@@ -50,7 +61,13 @@ const App = () => {
   const handleLogout = () => {
     window.localStorage.removeItem('user')
     dispatch(setUser(null))
+    history.push('/')
   }
+
+  const matchUser = useRouteMatch('/users/:id')
+  const displayUser = matchUser
+    ? users.find(user => user.id === String(matchUser.params.id))
+    : null
 
   if (user === null) {
     return (
@@ -83,8 +100,11 @@ const App = () => {
         />
 
         <Switch>
+          <Route path="/users/:id">
+            <User user={displayUser} />
+          </Route>
           <Route path="/users">
-            <Users />
+            <Users users={users} />
           </Route>
           <Route path="/">
             <Blogs />
--- part7/bloglist/client/src/components/Users.js
+++ part7/bloglist/client/src/components/Users.js
@@ -1,27 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import userService from '../services/users'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Users = () => {
-  const [users, setUsers] = useState([])
-
-  useEffect(async () => {
-    const users = await userService.getAll()
-    setUsers(users)
-  }, [])
-
+const Users = ({ users }) => {
   return (
     <div>
       <h2>Users</h2>
       <table>
-        <tr>
-          <th>User Names</th>
-          <th>Blogs Created</th>
-        </tr>
-        {
-          users.map(user => {
-            return <tr key={user.id}><td>{user.name}</td><td>{user.blogs.length}</td></tr>
-          })
-        }
+        <thead>
+          <tr>
+            <th>User Names</th>
+            <th>Blogs Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(user => {
+              return <tr key={user.id}><td><Link to={`/users/${user.id}`}>{user.name}</Link></td><td>{user.blogs.length}</td></tr>
+            })
+          }
+        </tbody>
 
       </table>
     </div>
